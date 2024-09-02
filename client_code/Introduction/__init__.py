d327bbc6d36524854b9ae8f745c5249d77da2e5f
muhammadhammad.javeed0@gmail.com <muhammadhammad.javeed0@gmail.com>
2024-09-02 10:34:36 +0000

    Created Success; Edited settings, Survey and Introduction; Deleted success, EU_page and UK_Page

--- client_code/Introduction/__init__.py
+++ client_code/Introduction/__init__.py
@@ -7,8 +7,7 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from ..EU_page import EU_page
-from ..UK_Page import UK_Page
+from ..Survey import Survey
 
 
 class Introduction(IntroductionTemplate):
@@ -18,25 +17,25 @@ class Introduction(IntroductionTemplate):
 
     # Any code you write here will run when the form opens.
 
-  def Region_dropdown_change(self, **event_args):
+  def ProjectModel_dropdown_change(self, **event_args):
     """This method is called when an item is selected"""
     pass
 
-  def Username_text_pressed_enter(self, **event_args):
+  def Clientname_text_pressed_enter(self, **event_args):
     """This method is called when the user presses Enter in this text box"""
     
     pass
 
-  def next_btn_click(self, **event_args):
+  def IntroNext_btn_click(self, **event_args):
     """This method is called when the button is clicked"""
-    region = self.Region_dropdown.selected_value
-    text = self.Username_text.text
-    project = self.Project_text.text
+    model = self.ProjectModel_dropdown.selected_value
+    client = self.Clientname_text.text
+    project = self.ProjectName_text.text
     
-    if region == "UK" or "Canada" or "USA" or "Europe":
-      open_form(UK_Page())
+    if model is not None:
+      open_form(Survey())
     else:
-      print("None")
+      print("Model Value Found = None")
       
-    print(f"This are the values Region {region}, text {text} and Project {project}")
+    print(f"This are the values Project Model: {model}\nClient Name: {client}\nProject Name {project}")
     
--- client_code/Survey/__init__.py
+++ client_code/Survey/__init__.py
@@ -7,44 +7,69 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from ..Success import Success
 
 class Survey(SurveyTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.check_boxes = [self.check_box_1, self.check_box_2, self.check_box_3, self.check_box_4]
-    self.slider_levels = {1: 'strongly disagree',
-                          2: 'disagree',
-                          3: 'slightly disagree',
-                          4: 'neutral',
-                          5: 'slightly agree',
-                          6: 'agree',
-                          7: 'strongly agree'}
+    
+    self.slider_levels = {1: 'No',
+                          2: 'Daily',
+                          3: 'Weekly',
+                          4: 'Monthly'}
     #set the slider to neutral initially
-    self.slider_1.level = 4
-    self.slider_label.text = self.slider_levels[self.slider_1.level]
+    self.Reporting_slider.level = 4
+    self.slider_label.text = self.slider_levels[self.Reporting_slider.level]
 
 
   def Reporting_slider_change(self, level, **event_args):
     """This method is called when the slider is moved"""
-    self.slider_label.text = self.slider_levels[self.slider_1.level]
+    self.slider_label.text = self.slider_levels[self.Reporting_slider.level]
 
   def submit_button_click(self, **event_args):
     """This method is called when the button is clicked"""
-    age = self.age_dropdown.selected_value
-    frequency = self.radio_button_1.get_group_value()
-    methods = [box.text for box in self.check_boxes if box.checked == True]
-    rating = self.slider_label.text
-    comments = self.comment_area.text
+    ## Page 1 or Section 1 Project Details
+    coverage = self.coverage_dropdown.selected_value
+    location = self.location_dropdown.selected_value
+    project_client = self.client_dropdown.selected_value\
+    ## Page 2 or Section 2 Technical Details
+    support_lvl = self.support_level_dropdown.selected_value
+    technology = self.project_tech_dropdown.selected_value
+    environment = self.project_env_dropdown.selected_value
+    # Project Yes n no
+    existing = self.projectex_RY.get_group_value()
+    ticketing = self.projecttick_RY.get_group_value()
+    # Project Reporting slider
+    reporting_lvl = self.slider_levels[self.Reporting_slider.level]
+    # Support Requirments
+    voice_sup = self.voice_sup_dropdown.selected_value
+    SLA = self.sla_dropdown.selected_value
+    monitoring_tool = self.monitoring_tool_dropdown.selected_value
+    ticket_count = self.ticket_count_dropdown.selected_value
+    medium = self.sup_medium_dropdown.selected_value
+    no_portals = self.number_portals_dropdown.selected_value
+    
+    # Additional Comments
+    feedback = self.feedback_area.text
     
     if age and frequency and methods and rating:
       anvil.server.call('add_responses', age, frequency, methods, rating, comments)
       alert("Thank you for submitting feedback!")
       get_open_form().content_panel.clear()
-      get_open_form().content_panel.add_component(Survey(), full_width_row=True)
+      get_open_form().content_panel.add_component(Success(), full_width_row=True)
 
     
-    else: alert("Please fill out required fields")
+    else:
+      alert("Please fill out required fields")
+
+  def coverage_dropdown_change(self, **event_args):
+    """This method is called when an item is selected"""
+    pass
+
+  def projecttick_RY_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    pass
       
       
     
